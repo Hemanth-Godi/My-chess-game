808edc89416ec3f9f5085f186cccb8497c91dd4e
Hemanth Godi <godihemanth589@gmail.com>
2026-07-11 15:33:43 +0530

    created a map for every piece to render

---
+++ js files/pieces-map.js
@@ -0,0 +1,14 @@
+export const piecesMap = {
+  br:"images/black-rook.png",
+  bn:"images/black-knight.png",
+  bb:"images/black-bishop.png",
+  bq:"images/black-queen.png",
+  bk:"images/black-king.png",
+  bp:"images/black-pawn.png",
+  wr:"images/white-rook.png",
+  wn:"images/white-knight.png",
+  wb:"images/white-bishop.png",
+  wq:"images/white-queen.png",
+  wk:"images/white-king.png",
+  wp:"images/white-pawn.png"
+}
