13799f5fd4125c48d42a8a08dbd6fbfdefdee113
Hemanth Godi <godihemanth589@gmail.com>
2026-07-13 12:40:55 +0530

    making pieces movable

--- js files/chess.js
+++ js files/chess.js
@@ -4,6 +4,8 @@ const play = document.querySelector(".play-area");
 for(let i=0;i<64;i++){
   const square = document.createElement("div");
   square.classList.add("square");
+  square.dataset.row = Math.floor(i/8);
+  square.dataset.col = i%8;
   play.appendChild(square);
 }
 const board = [
@@ -17,9 +19,11 @@ const board = [
   ["wr","wn","wb","wq","wk","wb","wn","wr"]
 ];
 
+function renderBoard(){
 const squares = document.querySelectorAll(".square");
 for(let i=0;i<8;i++){
   for(let j=0;j<8;j++){
+    squares[i*8+j].innerHTML = "";
     const piece = board[i][j];
     if(piece!=""){
       squares[i*8+j].innerHTML=`<img src="${piecesMap[piece]}" alt="${piece}"
@@ -27,4 +31,31 @@ for(let i=0;i<8;i++){
     }
   }
 }
+}
+renderBoard();
+
+let selected = null;
+const squares = document.querySelectorAll(".square");
+squares.forEach(square=>{
+  square.addEventListener("click",()=>{
+    const row = Number(square.dataset.row);
+    const col = Number(square.dataset.col);
+    const piece = board[row][col];
+    if(selected===null){
+      if(piece!=""){
+        selected = {
+          row,
+          col
+        };
+        console.log("Selected");
+      }
+    }
+    else{
+      board[row][col] = board[selected.row][selected.col];
+      board[selected.row][selected.col] = "";
+      selected = null;
+      renderBoard();
+    }
+  });
+});
 
